95adfcf19ced25c1fa4ed991866c35b516a3229d
Shane <44891132+Slogue242@users.noreply.github.com>
2019-10-19 19:23:46 +1300

    Update py-ahk.py

--- py-ahk.py
+++ py-ahk.py
@@ -7,37 +7,40 @@ import win32clipboard
 current = set()
 kb = Controller()
 
-# Function to be executed
 def execute():
 	try:
-		# Get's all current data in clipboard
 		win32clipboard.OpenClipboard()
 		data = win32clipboard.GetClipboardData()
 		win32clipboard.CloseClipboard()
-		#Starts split data so it can be used in the further
+
 		stripping = data.split("\n")[0]
 		get_name = stripping.split("Name: ")[-1]
 
 		lname = get_name.split(' ', 1)
 
 		em_list = []
-		#Add's Name to list so it will be easier to call later on
+
 		for item in lname:
 			strip_word = item.strip()
 			em_list.append(strip_word)
-		#Printing the name and such
-		print(f"{em_list[-1]}, {em_list[0]}\tLogue, Shane")
-		kb.type(f"{em_list[-1]}, {em_list[0]}\tLogue, Shane")
-		pass
+
+		cleaning = data.strip('\r')
+
+		kb.type(f"{em_list[-1]}, {em_list[0]}\t\t\t\t\t\tLogue, Shane\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t")
+		for item in cleaning:
+			clean = item.strip('\n')
+			kb.type(rf"{clean}")
+			pass
+		print(f"{data}")
 	except:
 		pass
 	
-#Looks for a certain key press and then runs the function
+
 def on_press(key):
 	if key == Key.end:
 		execute()
 	else:
 		pass
-#Starts keyboard listening
+
 with keyboard.Listener(on_press=on_press) as listener:
 	listener.join()
