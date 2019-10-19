9a5be156935fd1243b81297abb89bf26535716b0
Shane <44891132+Slogue242@users.noreply.github.com>
2019-10-20 12:51:19 +1300

    Update py-ahk.py

--- py-ahk.py
+++ py-ahk.py
@@ -7,7 +7,7 @@ import win32clipboard
 current = set()
 kb = Controller()
 
-def execute():
+def req():
 	try:
 		win32clipboard.OpenClipboard()
 		data = win32clipboard.GetClipboardData()
@@ -35,10 +35,40 @@ def execute():
 	except:
 		pass
 	
+def incident():
+	try:
+		win32clipboard.OpenClipboard()
+		data = win32clipboard.GetClipboardData()
+		win32clipboard.CloseClipboard()
+
+		stripping = data.split("\n")[0]
+		get_name = stripping.split("Name: ")[-1]
+
+		lname = get_name.split(' ', 1)
+
+		em_list = []
+
+		for item in lname:
+			strip_word = item.strip()
+			em_list.append(strip_word)
+
+		cleaning = data.strip('\r')
+
+		kb.type(f"{em_list[-1]}, {em_list[0]}\t\t\t\t\t\tLogue, Shane\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t")
+		for item in cleaning:
+			clean = item.strip('\n')
+			kb.type(rf"{clean}")
+			pass
+		print(f"{data}")
+	except:
+		pass
+
 
 def on_press(key):
-	if key == Key.end:
-		execute()
+	if key == Key.page_up:
+		req()
+	elif key == Key.page_down:
+		incident()
 	else:
 		pass
 
